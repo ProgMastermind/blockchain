13efff835b005ff0c1837a779813101fd18f02c2
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-03-05 09:48:52 +0530

    accounts api

--- app/services/node/handlers/v1/v1.go
+++ app/services/node/handlers/v1/v1.go
@@ -29,8 +29,8 @@ func PublicRoutes(app *web.App, cfg Config) {
 
 	// app.Handle(http.MethodGet, version, "/sample", pbl.Sample)
 	app.Handle(http.MethodGet, version, "/genesis/list", pbl.Genesis)
-	// app.Handle(http.MethodGet, version, "/accounts/list", pbl.Accounts)
-	// app.Handle(http.MethodGet, version, "/accounts/list/:account", pbl.Accounts)
+	app.Handle(http.MethodGet, version, "/accounts/list", pbl.Accounts)
+	app.Handle(http.MethodGet, version, "/accounts/list/:account", pbl.Accounts)
 	// app.Handle(http.MethodGet, version, "/tx/uncommitted/list", pbl.Mempool)
 	// app.Handle(http.MethodGet, version, "/tx/uncommitted/list/:account", pbl.Mempool)
 	// app.Handle(http.MethodPost, version, "/tx/submit", pbl.SubmitWalletTransaction)
